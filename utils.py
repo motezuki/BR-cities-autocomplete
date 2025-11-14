16aec5795f96fe3b6df5cc4495a86ff00872ce80
Fabio Motezuki <95715242+motezuki@users.noreply.github.com>
2025-11-14 17:37:24 +0000

    Adicionada funcao para substituir caracteres especiais por caracteres simples

--- utils.py
+++ utils.py
@@ -3,8 +3,16 @@ import unicodedata
 
 DB_FILE = "municipios_brasil.db"
 
+
 def connectar_db(db_path: str = DB_FILE) -> sqlite3.Connection:
     """Conecta ao banco de dados SQLite"""
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row  # Permite acessar colunas por nome
     return conn
+
+
+def normaliza_string(text: str) -> str:
+    if text is None:
+        return ""
+    nf = unicodedata.normalize("NFD", str(text))
+    return nf.encode("ascii", "ignore").decode("utf-8").lower()
